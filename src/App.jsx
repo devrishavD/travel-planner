a65d34fc5fd8b543c293c5696ee00686b608efe9
devrishavD <rishavdamofficial@gmail.com>
2025-09-10 20:41:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -23,26 +23,8 @@ const App = () => (
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
-<<<<<<< HEAD
     </BrowserRouter>
   </QueryClientProvider>
 );
-=======
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the and React logos to learn more
-      </p>
-    </>
-  )
-}
->>>>>>> 32310c5d4406fd2fdea467d1e6859018fab321e7
 
 export default App;
